c6c3e78550e8cced9241e7f0816f9f15362b382f
wtli <2277506759@qq.com>
2019-12-14 16:10:28 +0800

    redis配置及redis调用demo

--- dubbo-provider/src/main/java/com/lzy/dubboprovider/server/DubboProvider.java
+++ dubbo-provider/src/main/java/com/lzy/dubboprovider/server/DubboProvider.java
@@ -7,8 +7,8 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.lzy.dubboapi.api.IDubboProvider;
 import com.lzy.dubboapi.api.Response;
 import com.lzy.mongodb.MongoDbUtils;
+import com.lzy.redis.JedisUtil;
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Service(
         version = "1.0.0",
@@ -18,13 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 )
 public class DubboProvider implements IDubboProvider {
 
-
     @Override
     public Response sayHello() {
 
         MongoDbUtils mongoDbUtils=new MongoDbUtils();
     Document  document=mongoDbUtils.findById("collection_test","5df32ee4dc1956535c6464d3");
-
+//        JedisUtil.setString("lwt","hello");
+//        return new Response(JedisUtil.getString("lwt"));
         return new Response(document.getString("age"));
     }
 }
