eda2a2645034902211d332100091949c741aaf70
chenzen94 <260386552@qq.com>
2019-12-23 11:55:22 +0800

    jwt+security

--- dubbo-consumer/src/main/java/com/lzy/consumer/utils/JwtTokenUtils.java
+++ dubbo-consumer/src/main/java/com/lzy/consumer/utils/JwtTokenUtils.java
@@ -23,7 +23,7 @@ public class JwtTokenUtils {
     private static final String ROLE_CLAIMS = "rol";
 
     // 过期时间是3600秒，既是1个小时
-    private static final long EXPIRATION = 60L;
+    private static final long EXPIRATION = 3600L;
 
     // 选择了记住我之后的过期时间为7天
     private static final long EXPIRATION_REMEMBER = 604800L;
